49bc2b10e20b6492f35e26f9204e8829dbe63c97
kavyajayan <kavyakjayachandran.mec@gmail.com>
2017-04-23 19:30:05 +0530

    Some more errors rectified

--- dbconnect.py
+++ dbconnect.py
@@ -6,11 +6,15 @@ def insert(uname,pwd):
 	con1 = sqlite3.connect(DATABASE)
 	cur1 = con1.cursor()
 	cur1.execute("INSERT INTO LOGIN (username,password) values (?,?)", (uname,pwd))
+	con1.commit()
+	con1.close()
 
 def fetch():
 	con2 = sqlite3.connect(DATABASE)
-	cur2 = con2.cursor()	
+	cur2 = con2.cursor()
+	cur2.execute("SELECT username, password from LOGIN")	
 	data=cur2.fetchall()
+	con2.close()
 	return data
 
 
